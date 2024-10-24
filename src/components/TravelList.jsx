88c2156d8ce4c3253b8ee4081aff64964d731a3c
Moaz Mubaydin <mmubaydin@gmail.com>
2024-10-24 08:44:31 +0200

    fixed cards aliginment

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -4,7 +4,7 @@ import TravelPlanCard from "./TravelPlanCard";
 export default function TravelList() {
   return travelPlanData.map((plan) => {
     return (
-      <div key={plan.id}>
+      <div key={plan.id} className="cards">
         <TravelPlanCard card={plan} />
       </div>
     );
